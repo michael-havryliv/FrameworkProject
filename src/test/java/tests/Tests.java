3aa0d0ff7016477e0edd0f646111f2157f68f0e0
Mykhailo Havryliv <michaelplusyou@gmail.com>
2021-10-30 18:25:19 +0300

    added test runner

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -1,10 +1,6 @@
 package tests;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
 
