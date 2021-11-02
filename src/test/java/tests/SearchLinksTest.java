c2ac81152fb62873628579a0c33d749b5246c50b
Mykhailo Havryliv <michaelplusyou@gmail.com>
2021-11-02 15:38:57 +0200

    added logs, bugs fixed

---
+++ src/test/java/tests/SearchLinksTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import java.util.logging.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SearchLinksTest extends BaseTest{
+
+    Logger logger = Logger.getLogger(SearchLinksTest.class.getName());
+
+    private final int DEFAULT_TIME_TO_WAIT = 30;
+
+    private final String SEARCHED_KEYWORD = "чайник";
+    private final String HOZSKLAD_SITE = "hozsklad";
+    private final String HOZSKLAD_EXPECTED_PRODUCT = "ЧАЙНИК";
+
+    @Test
+    public void checkIfHozskladSiteContainsExpectedProducts() throws IOException {
+        getGoogleHomePage().inputSearchedKeywordInInputField(SEARCHED_KEYWORD);
+        getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+        logger.info("Google home page loaded");
+        boolean pageIsFound = false;
+        int currentPage = 0;
+        while(currentPage < 6){
+            logger.info("Current page: " + currentPage);
+            int currentSite = 0;
+            for (WebElement webElement : getGoogleResultsPage().getResultUrls()) {
+                if (webElement.getText().contains(HOZSKLAD_SITE)) {
+                    getGoogleResultsPage().clickOnLinkOfCurrentSite(currentSite);
+                    getSiteResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+                    pageIsFound = true;
+                    logger.info("Site is found.");
+                    break;
+                } else currentSite++;
+            }
+            if(!pageIsFound){
+                logger.info("Site is not found.");
+                getGoogleResultsPage().goToNextPage(currentPage);
+                getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+                currentPage++;
+            }else{
+                FileWriter nFile = new FileWriter("src\\test\\resources\\file1.txt");
+                for (WebElement webElement : getSiteResultsPage().getHozskladProductTitles()){
+                    Assert.assertTrue("Site does not have searched product.",webElement.getText().contains(HOZSKLAD_EXPECTED_PRODUCT));
+                    nFile.write(webElement.getAttribute("href")+"\n");
+                }
+                logger.info("Links saved to file. End of test.");
+                nFile.close();
+                break;
+            }
+        }
+        Assert.assertTrue("Page was not found. End of test.",pageIsFound);
+    }
+}
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -6,13 +6,16 @@ import org.openqa.selenium.WebElement;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class SearchTests extends BaseTest{
+
+    Logger logger = Logger.getLogger(SearchLinksTest.class.getName());
 
     private final int DEFAULT_TIME_TO_WAIT = 30;
 
     private final String SEARCHED_KEYWORD = "чайник";
-    private final String FOXTROT_SITE = "foxtrot";
+    private final String FOXTROT_SITE = "foxtrot9999999999";
     private final String EPICENTER_SITE = "epicentrk";
     private final String HOZSKLAD_SITE = "hozsklad";
     private final String FOXTROT_EXPECTED_PRODUCT = "Електрочайник";
@@ -23,41 +26,49 @@ public class SearchTests extends BaseTest{
     public void checkIfFoxtrotSiteContainsExpectedProducts(){
         getGoogleHomePage().inputSearchedKeywordInInputField(SEARCHED_KEYWORD);
         getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+        logger.info("Google home page loaded");
         boolean pageIsFound = false;
         int currentPage = 0;
-        while(true){
+        while(currentPage < 6){
+            logger.info("Current page: " + currentPage);
             int currentSite = 0;
             for (WebElement webElement : getGoogleResultsPage().getResultUrls()) {
                 if (webElement.getText().contains(FOXTROT_SITE)) {
                     getGoogleResultsPage().clickOnLinkOfCurrentSite(currentSite);
                     getSiteResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
                     pageIsFound = true;
+                    logger.info("Site is found");
                     break;
                 } else currentSite++;
             }
             if(!pageIsFound){
                 getGoogleResultsPage().goToNextPage(currentPage);
                 getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+                logger.info("Site is not found");
                 currentPage++;
             }else break;
         }
         for (WebElement webElement : getSiteResultsPage().getFoxtrotProductTitles()){
-            Assert.assertTrue(webElement.getText().contains(FOXTROT_EXPECTED_PRODUCT));
+            Assert.assertTrue("Site does not have searched product",webElement.getText().contains(FOXTROT_EXPECTED_PRODUCT));
         }
+        Assert.assertTrue("Site was not found. End of test.",pageIsFound);
     }
 
     @Test
     public void checkIfEpicenterSiteContainsExpectedProducts(){
         getGoogleHomePage().inputSearchedKeywordInInputField(SEARCHED_KEYWORD);
         getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+        logger.info("Google home page loaded");
         boolean pageIsFound = false;
         int currentPage = 0;
-        while(true){
+        while(currentPage < 6){
+            logger.info("Current page: " + currentPage);
             int currentSite = 0;
             for (WebElement webElement : getGoogleResultsPage().getResultUrls()) {
                 if (webElement.getText().contains(EPICENTER_SITE)) {
                     getGoogleResultsPage().clickOnLinkOfCurrentSite(currentSite);
                     getSiteResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+                    logger.info("Site is found");
                     pageIsFound = true;
                     break;
                 } else currentSite++;
@@ -65,43 +76,14 @@ public class SearchTests extends BaseTest{
             if(!pageIsFound){
                 getGoogleResultsPage().goToNextPage(currentPage);
                 getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
+                logger.info("Site is not found");
                 currentPage++;
             }else break;
         }
         for (WebElement webElement : getSiteResultsPage().getEpicenterProductTitles()){
-            Assert.assertTrue(webElement.getText().contains(EPICENTER_EXPECTED_PRODUCT));
+            Assert.assertTrue("Site does not have searched product",webElement.getText().contains(EPICENTER_EXPECTED_PRODUCT));
         }
-    }
-
-    @Test
-    public void checkIfHozskladSiteContainsExpectedProducts() throws IOException {
-        getGoogleHomePage().inputSearchedKeywordInInputField(SEARCHED_KEYWORD);
-        getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
-        boolean pageIsFound = false;
-        int currentPage = 0;
-        while(true){
-            int currentSite = 0;
-            for (WebElement webElement : getGoogleResultsPage().getResultUrls()) {
-                if (webElement.getText().contains(HOZSKLAD_SITE)) {
-                    getGoogleResultsPage().clickOnLinkOfCurrentSite(currentSite);
-                    getSiteResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
-                    pageIsFound = true;
-                    break;
-                } else currentSite++;
-            }
-            if(!pageIsFound){
-                getGoogleResultsPage().goToNextPage(currentPage);
-                getGoogleResultsPage().waitForPageLoadComplete(DEFAULT_TIME_TO_WAIT);
-                currentPage++;
-            }else break;
-        }
-
-        FileWriter nFile = new FileWriter("src\\test\\resources\\file1.txt");
-        for (WebElement webElement : getSiteResultsPage().getHozskladProductTitles()){
-            Assert.assertTrue(webElement.getText().contains(HOZSKLAD_EXPECTED_PRODUCT));
-            nFile.write(webElement.getAttribute("href")+"\n");
-        }
-        nFile.close();
+        Assert.assertTrue("Site was not found. End of test.",pageIsFound);
     }
 
 }
